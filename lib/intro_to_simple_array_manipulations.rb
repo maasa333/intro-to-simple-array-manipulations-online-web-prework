a1b41a630f4865059d4df098a8068c8a3599af87
Maasa Kono <konomaasa@gmail.com>
2019-04-28 23:29:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,7 @@ end
 
 def using_pop(array)
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-
+  delete_string = array.pop
 end
 
 def pop_with_args(array)
