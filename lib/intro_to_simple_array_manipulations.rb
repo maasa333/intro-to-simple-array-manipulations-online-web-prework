7f4308186786c226759f50438930163a4eec7929
Maasa Kono <konomaasa@gmail.com>
2019-04-28 23:20:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,8 @@ def using_push(array, string)
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   updated_array = array.push("Niger")
 end
+
+def using_unshift(array, string)
+  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  updated_array = array.unshift("Brooklyn Heights")
+end
